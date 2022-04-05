0b111af95ad45d572100c18c587a730cae11daea
SIRIEZ-Axel <axel.siriez@gmail.com>
2022-04-05 15:37:28 +0200

    less useless info

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+const API_KEY = 'api_key=d645245eff2b1f60d18bc1ccf08032d2';
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const API_URL = BASE_URL + '/discover/movie/?certification_country=US&certification=R&sort_by=vote_average.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
@@ -12,26 +12,7 @@ function getMovies(url) {
         console.log(data.results)
         if(data.results.length !== 0){
             showMovies(data.results);
-            currentPage = data.page;
-            nextPage = currentPage + 1;
-            prevPage = currentPage - 1;
-            totalPages = data.total_pages;
-
-            current.innerText = currentPage;
-
-            if(currentPage <= 1){
-              prev.classList.add('disabled');
-              next.classList.remove('disabled')
-            }else if(currentPage>= totalPages){
-              prev.classList.remove('disabled');
-              next.classList.add('disabled')
-            }else{
-              prev.classList.remove('disabled');
-              next.classList.remove('disabled')
-            }
-
-            tagsEl.scrollIntoView({behavior : 'smooth'})
-
+            
         }else{
             main.innerHTML= `<h1 class="no-results">No Results Found</h1>`
         }
@@ -52,15 +33,14 @@ function showMovies(data) {
 
             <div class="movie-info">
                 <h3>${title}</h3>
-                <span class="${getColor(vote_average)}">${vote_average}</span>
+                <span class="${(vote_average)}">${vote_average}</span>
             </div>
 
             <div class="overview">
-
                 <h3>Overview</h3>
                 ${overview}
                 <br/> 
-                <button class="know-more" id="${id}">Know More</button
+                <button class="know-more" id="${id}">Know More</button>
             </div>
         
         `
@@ -74,12 +54,3 @@ function showMovies(data) {
     })
 }
 
-function getColor(vote) {
-    if(vote>= 8){
-        return 'green'
-    }else if(vote >= 5){
-        return "orange"
-    }else{
-        return 'red'
-    }
-}
