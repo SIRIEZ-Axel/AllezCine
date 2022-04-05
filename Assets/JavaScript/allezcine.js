3e668b43ddd4407ed0f818b429f5dc9031334758
Nadison <nadisonsolojeanfidelys@gmail.com>
2022-04-05 16:43:19 +0200

    petit à petit

---
+++ Assets/JavaScript/allezcine.js
@@ -0,0 +1,42 @@
+const API_KEY = 'api_key=d645245eff2b1f60d18bc1ccf08032d2';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w185';
+const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+
+getMovies(API_URL);
+
+function getMovies(url) {
+  lastUrl = url;
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results)
+        if(data.results.length !== 0){
+            showMovies(data.results);
+            
+        }else{
+            main.innerHTML= `<h1 class="no-results">No Results Found</h1>`
+        }
+    })
+}
+
+function showMovies(data) {
+    main.innerHTML = '';
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview, id} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+             <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        
+        `
+        main.appendChild(movieEl);
+    })
+}
