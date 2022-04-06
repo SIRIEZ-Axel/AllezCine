102e70456c452bab81e773062695521f47be78ad
Nadison Fidely <fidelysnad@exemple.com>
2022-04-06 15:32:10 +0200

    image net

--- Assets/JavaScript/allezcine.js
+++ Assets/JavaScript/allezcine.js
@@ -1,7 +1,7 @@
-const API_KEY = 'api_key=d645245eff2b1f60d18bc1ccf08032d2';
+const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
-const IMG_URL = 'https://image.tmdb.org/t/p/w185';
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
 getMovies(API_URL);
