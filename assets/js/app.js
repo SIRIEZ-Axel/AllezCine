edd662e2cefbe2eebeaff50b7d14aefa303cc8fd
SIRIEZ-Axel <axel.siriez@gmail.com>
2022-04-07 16:08:26 +0200

    add css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 const API_KEY = 'api_key=d645245eff2b1f60d18bc1ccf08032d2';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
-const IMG_URL = 'https://image.tmdb.org/t/p/w185';
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
 getMovies(API_URL);
@@ -12,7 +12,6 @@ function getMovies(url) {
         console.log(data.results)
         if(data.results.length !== 0){
             showMovies(data.results);
-            
         }else{
             main.innerHTML= `<h1 class="no-results">No Results Found</h1>`
         }
@@ -22,11 +21,11 @@ function getMovies(url) {
 function showMovies(data) {
     main.innerHTML = '';
     data.forEach(movie => {
-        const {title, poster_path, vote_average, overview, id} = movie;
-        const movieEl = document.createElement('div');
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieEl = document.createElement('span');
         movieEl.classList.add('movie');
         movieEl.innerHTML = `
-             <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+            <img class="movie-poster" src="${poster_path? IMG_URL + poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
 
             <div class="movie-info">
                 <span class="${(vote_average)}">${vote_average}</span>
@@ -34,9 +33,9 @@ function showMovies(data) {
 
             <div class="overview">
                 <h3>Overview</h3>
-                ${overview}
+                <p class="movie-overview">${overview}</p>
+                <button class="know-more-btn" id="over-btn">Know more</button>
             </div>
-        
         `
         main.appendChild(movieEl);
     })
